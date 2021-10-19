8ca2f0c8be1f6947ae6e0a1bc1d5e2a7295c4efa
pl1824 <patricktonylee@gmail.com>
2021-10-19 11:40:51 -0400

    testing

--- API/endpoints.py
+++ API/endpoints.py
@@ -3,12 +3,16 @@ This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
 
+from http import HTTPStatus
 from flask import Flask
 from flask_restx import Resource, Api
-import db.db as db
+# import db.db as db
 
 app = Flask(__name__)
 api = Api(app)
+
+HELLO = 'Hola'
+WORLD = 'mundo'
 
 
 @api.route('/hello')
@@ -22,7 +26,7 @@ class HelloWorld(Resource):
         A trivial endpoint to see if the server is running.
         It just answers with "hello world."
         """
-        return {'hello': 'world'}
+        return {HELLO: WORLD}
 
 
 @api.route('/endpoints')
@@ -39,13 +43,14 @@ class Endpoints(Resource):
         return {"Available endpoints": endpoints}
 
 
-@api.route('/pets')
-class Pets(Resource):
+@api.route('/create_user/<username>')
+class CreateUser(Resource):
     """
-    This class supports fetching a list of all pets.
+    This class supports adding a user to the chat room.
     """
-    def get(self):
+    @api.response(HTTPStatus.OK, 'Success')
+    def post(self, username):
         """
-        This method returns all pets.
+        This method adds a user to the chatroom.
         """
-        return db.fetch_pets()
+        return username
--- API/tests/test_endpoints.py
+++ API/tests/test_endpoints.py
@@ -1,11 +1,22 @@
-from unittest import TestCase, skip
+"""
+This file holds the tests for endpoints.py.
+"""
+
+from unittest import TestCase, skip 
+from flask_restx import Resource, Api
 
 import API.endpoints as ep
 
-class EndpointTestCast(TestCase):
+
+class EndpointTestCase(TestCase):
     def setUp(self):
         pass
-    def testDown(self):
+
+    def tearDown(self):
         pass
-    def test_self_assert(self):
-        self.assertTrue(False)
+
+    def test_hello(self):
+        hello = ep.HelloWorld(Resource)
+        ret = hello.get()
+        self.assertIsInstance(ret, dict)
+        self.assertIn(ep.HELLO, ret)
