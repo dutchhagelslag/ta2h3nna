a19dc9362bc1d6bc760a2eeb04aeec9e09e1c8d6
pl1824 <patricktonylee@gmail.com>
2021-12-18 23:03:12 -0500

    testing

--- db/mongo_test.py
+++ db/mongo_test.py
@@ -1,6 +1,26 @@
 import pymongo as pm
 
 client = pm.MongoClient()
-print(client)
-db = client["DesignsDB"]
-print(db)
+
+# do something about hardcoded password later
+# client = pm.MongoClient("mongodb+srv://tattoo:tattoo101@cluster0.tattm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# db = client.test
+
+DB_NAME = 'tattooDB'
+DESIGN  = 'sample_mflix'
+FONTS = 'fonts'
+
+print(f"{client=}")
+
+designCollection = client[DB_NAME][DESIGN]
+
+insert_ret = designCollection.insert_one({'fld':'value'})
+print(f"{insert_ret=}")
+
+doc = designCollection.find()
+
+for doc in doc:
+    print(f"{doc=}")
+
+remove = designCollection.delete_one({'fld':'value'})
+print(f"{remove=}")
